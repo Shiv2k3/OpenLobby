ab24737c6ddd102b7e704639fc10155c27b39bd9
Shiv2k3 <78758913+Shiv2k3@users.noreply.github.com>
2024-06-01 23:39:35 -0500

    add: mediocre server

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
-﻿Console.WriteLine("Hello, I'm am server, what is you're name?");
-string? name = Console.ReadLine();
-if (string.IsNullOrWhiteSpace(name))
-{
-    Console.WriteLine("You didn't enter a valid name!");
-}
-else
-{
-    Console.WriteLine("Hello " + name + ", welcome to the good side!");
-}
+﻿using Microsoft.Extensions.Configuration;
+using System.Net;
+using System.Net.Sockets;
+
+Console.WriteLine("Listener socket is being setup");
+
+IConfiguration config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+string portStr = config.GetRequiredSection("LISTEN_PORT").Value;
+int port = int.Parse(portStr);
+IPEndPoint lep = new(IPAddress.Any, port);
+Socket listener = new(SocketType.Stream, ProtocolType.Tcp);
+listener.Bind(lep);
+listener.Listen();
+
+Console.WriteLine("Listener socket has been setup");
+Console.ReadKey();
+listener.Close();
