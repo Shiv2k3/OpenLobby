e361aab24281db474e4604668ed002f1cd08ecf1
Shiv2k3 <78758913+Shiv2k3@users.noreply.github.com>
2024-06-01 23:13:27 -0500

    add greet

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, I'm the server");
+﻿Console.WriteLine("Hello, I'm am server, what is you're name?");
+string? name = Console.ReadLine();
+if (string.IsNullOrWhiteSpace(name))
+{
+    Console.WriteLine("You didn't enter a valid name!");
+}
+else
+{
+    Console.WriteLine("Hello " + name + ", welcome to the good side!");
+}
